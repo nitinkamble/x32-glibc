3f053d35939b47727e7bb45fb63e13018c356c4e
H.J. Lu <hjl.tools@gmail.com>
2011-03-04 20:52:54 -0800

    Put back 8byte CIE data alignment factor.

--- sysdeps/unix/sysv/linux/x86_64/sigaction.c
+++ sysdeps/unix/sysv/linux/x86_64/sigaction.c
@@ -140,14 +140,14 @@ asm									\
    "	.byte 1\n"	/* Version number */				\
    "	.string \"zRS\"\n" /* NUL-terminated augmentation string */	\
    "	.uleb128 1\n"	/* Code alignment factor */			\
-   "	.sleb128 -" LP_SIZE "\n" /* Data Alignment Factor */	\
+   "	.sleb128 -8\n"	/* Data alignment factor */			\
    "	.uleb128 16\n"	/* Return address register column (rip) */	\
    /* Augmentation value length */					\
    "	.uleb128 .LENDAUGMNT_" #name "-.LSTARTAUGMNT_" #name "\n"	\
    ".LSTARTAUGMNT_" #name ":\n"						\
    "	.byte 0x1b\n"	/* DW_EH_PE_pcrel|DW_EH_PE_sdata4. */		\
    ".LENDAUGMNT_" #name ":\n"						\
-   "	.align " LP_SIZE "\n"					\
+   "	.align 8\n"							\
    ".LENDCIE_" #name ":\n"						\
    "	.long .LENDFDE_" #name "-.LSTARTFDE_" #name "\n" /* FDE len */	\
    ".LSTARTFDE_" #name ":\n"						\
@@ -178,7 +178,7 @@ asm									\
    /* do_expr (49 |* rflags *|, oEFL) */				\
    /* `cs'/`ds'/`fs' are unaligned and a different size.  */		\
    /* gas: Error: register save offset not a multiple of 8  */		\
-   "	.align " LP_SIZE "\n"					\
+   "	.align 8\n"							\
    ".LENDFDE_" #name ":\n"						\
    "	.previous\n"							\
    );
